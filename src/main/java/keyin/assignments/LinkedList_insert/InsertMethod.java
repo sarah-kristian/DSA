462bfa5a4008fa940a96f73368fe18e4625c0cba
Sarah Kristian Perry <sarah.perry@keyin.com>
2025-07-16 16:36:26 -0230

    Refactor LinkedList_insert to improve method naming & add constructors:
    - Renamed methods for clarity (e.g., `insertLinkedList` → `insertNode`, `deleteLinkedList` → `deleteNode`, etc.).
    - Added parameterized constructors in the `Node` class.
    - Consolidated redundant logic in linked list insertion methods for improved readability and maintainability.
    - Adjusted `UndoRedoManager` package name for consistency.

--- src/main/java/keyin/assignments/LinkedList_insert/InsertMethod.java
+++ src/main/java/keyin/assignments/LinkedList_insert/InsertMethod.java
@@ -5,7 +5,7 @@ public class InsertMethod {
     public Node tail;
     public int size;
 
-    public Node createSingleLinkedlist(int nodeValue){
+    public Node createSingleLinkedList(int nodeValue){
         Node node = new Node();
         node.next = null;
         node.value = nodeValue;
@@ -15,6 +15,31 @@ public class InsertMethod {
         return head;
     }
 
+    public void insertNode(int nodeValue, int location) {
+        Node node = new Node(nodeValue);
 
+        if (head == null){
+            createSingleLinkedList(nodeValue);
+            return;
+        }
+        if (location == 0) {
+            node.next = head;
+            head = node;
+        } else if (location >= size){
+            tail.next = node;
+            tail = node;
+        } else {
+            Node tempNode = head;
+            int index = 0;
+            while (index < location - 1){
+                tempNode = tempNode.next;
+                index++;
+            }
+            Node nextNode = tempNode.next;
+            tempNode.next = node;
+            node.next = nextNode;
+        }
+        size++;
+    }
 
 }
--- src/main/java/keyin/assignments/LinkedList_insert/SingleLinkedListDemo.java
+++ src/main/java/keyin/assignments/LinkedList_insert/SingleLinkedListDemo.java
@@ -5,10 +5,10 @@ public class SingleLinkedListDemo {
     public static void main(String[] args) {
         AllMethods sll = new AllMethods();
         sll.traverseLinkedList();
-        sll.insertLinkedList(1,0);
-        sll.insertLinkedList(2,1);
-        sll.insertLinkedList(3,2);
-        sll.insertLinkedList(4,3);
+        sll.insertNode(1,0);
+        sll.insertNode(2,1);
+        sll.insertNode(3,2);
+        sll.insertNode(4,3);
         sll.traverseLinkedList();
         sll.searchNode(4);
 
